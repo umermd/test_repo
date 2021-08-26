19e8414fe7339464c395dd8a211d07342d0cb7c5
umermd <32034096+umermd@users.noreply.github.com>
2021-08-26 19:30:41 +0500

    Created hello_test file to check github

---
+++ hello_test.py
@@ -0,0 +1,7 @@
+#This is a test file to check github feature branch
+def hello_github(user):
+  print('Hello {user}, Welcome to github.'.format(user=user))
+ 
+if __main__=="__main__":
+  hello_github('umer')
+  
